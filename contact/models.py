5751a03eb3e01136a54bcdb369233e9b09203479
Joao Rafael <joaorafaelavancinibaiao4@gmail.com>
2024-12-15 13:46:38 +0100

    commit

--- contact/models.py
+++ contact/models.py
@@ -11,7 +11,12 @@ from django.utils import timezone
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
+
     def __str__(self) -> str:
         return self.name
 
