4e306ace0f974be1e0a08bb81e9eeef664662859
Joao Rafael <joaorafaelavancinibaiao4@gmail.com>
2024-12-17 11:28:27 +0100

    commit

--- contact/admin.py
+++ contact/admin.py
@@ -5,7 +5,7 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'show,'
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id',
     list_filter = 'created_date',
     search_fields = 'id', 'firs_name', 'last_name',
