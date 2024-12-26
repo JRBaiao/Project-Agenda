cde8691576ccc07096e3e195c5cfd838aa83dd9f
Joao Rafael <joaorafaelavancinibaiao4@gmail.com>
2024-12-26 13:31:44 +0100

    commit

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -9,7 +9,7 @@ def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
 
         context = {
             'form': form,
@@ -45,7 +45,7 @@ def update(request, contact_id):
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
 
         context = {
             'form': form,
@@ -79,9 +79,11 @@ def delete(request, contact_id):
         Contact, pk=contact_id, show=True
     )
     confirmation = request.POST.get('confirmation', 'no')
+
     if confirmation == 'yes':
         contact.delete()
         return redirect('contact:index')
+
     return render(
         request,
         'contact/contact.html',
